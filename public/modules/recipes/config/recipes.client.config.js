b6f78b7af4df59c049145e28e4b1dd6fc10fd3dd
Joshua Kennedy <josh@ponycode.com>
2015-01-16 09:29:37 -0800

    removing unused stuff

--- public/modules/recipes/config/recipes.client.config.js
+++ public/modules/recipes/config/recipes.client.config.js
@@ -4,8 +4,8 @@
 angular.module('recipes').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Recipes', 'recipes', 'dropdown', '/recipe(/create)?');
-		Menus.addSubMenuItem('topbar', 'recipes', 'List Recipes', 'recipes');
-		Menus.addSubMenuItem('topbar', 'recipes', 'New Recipe', 'recipes/create');
+		//Menus.addMenuItem('topbar', 'Recipes', 'recipes', 'dropdown', '/recipe(/create)?');
+		//Menus.addSubMenuItem('topbar', 'recipes', 'List Recipes', 'recipes');
+		//Menus.addSubMenuItem('topbar', 'recipes', 'New Recipe', 'recipes/create');
 	}
 ]);
